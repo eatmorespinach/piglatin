213bf522d3f58a9448fe9d8d6150f2bfc7c305da
Drew Sing <sing.drew@gmail.com>
2013-09-19 23:29:16 -0700

    redid piglatin with new methods

--- piglatin.rb
+++ piglatin.rb
@@ -1,86 +1,41 @@
-#iterate through each letter of the word
-#stop once you hit a vowel
-#move vowel to end of the word
-#add ay to end
-#if word starts with a vowel, don't change it
-#puts "type word here!"
+class Piglatin
 
-#define the alphabet with alpha array, vowel array, and alpha - vowels to equal consonants
-#if vowel is first word, add 'ay to string'
-#elsif 
+  def initialize(word)
+    @word = word
+    @vowels = ["a","e","i","o","u"]
+    @first_half = []
+  end
 
-
-class PiglatinConverter
-
-  def self.convert(str)
-  
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-  
-
-    consonants = alpha - vowels
-
-    if vowels.include?(str[0])
-      str + 'ay'
-
-    elsif consonants.include?(str[1]) && consonants.include?(str[2])
-      return str[3..-1] + @str[0..2] + 'ay'
-
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
-      return str[2..-1] + str[0..1] + 'ay'
+  def word
+    word_array = @word.split(//)
     
-    elsif consonants.include?(str[0])
-      return str[1..-1] + str[0] + 'ay'
-    else
-      return "Please enter a proper word. Numbers are not accepted"
-    end
+    if @vowels.include?(word_array[0])
+      word_array << "way"
+      puts word_array.join("").inspect
+    
+    else 
+      word_array.each_with_index do |letter, index|
+      
+      unless @vowels.include?(word_array[index]) 
+        @first_half << letter
+      end
+
+        if @vowels.include?(word_array[index])
+          word_array.shift(index)
+          word_array << @first_half << "ay"
+          puts word_array.join("").inspect
+          break
+        end
+      end
+    end 
   end
 end
 
-puts PiglatinConverter.convert("table")
 
+animal = Piglatin.new("animal")
+featherword = Piglatin.new("camel")
+test = Piglatin.new("fffffffffffeather")
 
-
-
-
-
-
-# puts pigword = "kentucky".split("")
-
-# alpha = ('a'..'z').to_a
-#   vowels = ['a', 'e', 'i', 'o', 'u']
-#   consonants = alpha - vowels
-
-# if vowels.include?(str[0])
-#   str + 'ay'
-#   elsif consonants.include?(str[0]) && consonants.include?(str[1])
-#     str[2..-1] + str[0..1] + 'ay'
-#   elsif consonants.include?(str[0])
-#     str[1..-1] + str[0] + 'ay'
-#   else
-#     str # return unchanged
-#   end
-
-
-#puts pigword = gets.chomp.downcase
-
-#save beg words into an array, then append them to the end. 
-
-# consonant = []
-
-# #vowels = ["a", "e", "i", "o", "u"]
-
-
-# pigword.include?(vowels)
-
-# pigword.each_with_index do |element, index|
-
-# end
-
-# class PiglatinConverter
-
-#   def self.convert(word)
-#     # Your code goes here
-#   end
-
-# end
+animal.word
+featherword.word
+test.word
